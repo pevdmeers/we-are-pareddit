ab592605bcb5740de2ccd57ec3d7f6b0aae66bfd
Peter Vandermeersch <pevdmeers@gmail.com>
2015-11-30 22:44:20 +0100

    Basic functionalities finished :D

--- we-are-pareddit.js
+++ we-are-pareddit.js
@@ -1,4 +1,7 @@
+TopicList = new Mongo.Collection("topics");
+
 if (Meteor.isClient) {
+/*
   // counter starts at 0
   Session.setDefault('counter', 0);
 
@@ -14,10 +17,49 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+  */
+  
+  Template.topiclist.helpers({
+	'topic': function() {
+		return TopicList.find();
+	}
+  });
+  
+  Template.topicview.events({
+	'click .remove': function() {
+		console.log("removing " + this._id);
+		TopicList.remove(this._id);
+	},
+	'click h4 .dislike': function() {
+		console.log("disliking " + this._id);
+		TopicList.update(this._id, {$inc: {dislikes: -1} });
+	},
+	'click h4 .like': function() {
+		console.log("liking " + this._id);
+		TopicList.update(this._id, {$inc: {dislikes: +1} });
+	}
+  });
+    
+  Template.newTopic.events({
+    'submit form': function (event) {
+	    event.preventDefault();
+	    TopicList.insert({
+		    'title': event.target.topicTitle.value,
+		    'description': event.target.topicDescription.value,
+		    'dislikes': 0
+	    });
+	    event.target.topicTitle.value = "";
+	    event.target.topicDescription.value = "";
+    }
+  });
+
 }
 
 if (Meteor.isServer) {
+  /*
   Meteor.startup(function () {
     // code to run on server at startup
   });
+  */
 }
+
