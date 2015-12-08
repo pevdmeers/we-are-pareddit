314599f94f9003aacb13491a54f78dc3cdaafcee
pevdmeers <pevdmeers@gmail.com>
2015-12-08 22:27:37 +0100

    modify posts + layout

--- we-are-pareddit.js
+++ we-are-pareddit.js
@@ -18,48 +18,72 @@ if (Meteor.isClient) {
     }
   });
   */
-  
-  Template.topiclist.helpers({
-	'topic': function() {
-		return TopicList.find();
-	}
-  });
-  
-  Template.topicview.events({
-	'click .remove': function() {
-		console.log("removing " + this._id);
-		TopicList.remove(this._id);
-	},
-	'click h4 .dislike': function() {
-		console.log("disliking " + this._id);
-		TopicList.update(this._id, {$inc: {dislikes: -1} });
-	},
-	'click h4 .like': function() {
-		console.log("liking " + this._id);
-		TopicList.update(this._id, {$inc: {dislikes: +1} });
-	}
-  });
-    
-  Template.newTopic.events({
-    'submit form': function (event) {
-	    event.preventDefault();
-	    TopicList.insert({
-		    'title': event.target.topicTitle.value,
-		    'description': event.target.topicDescription.value,
-		    'dislikes': 0
-	    });
-	    event.target.topicTitle.value = "";
-	    event.target.topicDescription.value = "";
-    }
-  });
 
+	Template.topiclist.helpers({
+		'topic': function() {
+			return TopicList.find();
+		}
+	});
+
+	Template.topicview.events({
+		'click h4 .remove': function() {
+			console.log("removing " + this._id);
+			TopicList.remove(this._id);
+		},
+		'click h4 .edit': function(event) {
+			console.log("editing " + this._id);
+			var div = event.target.parentElement.parentElement;
+			$(div).addClass("hidden");
+			$(div).next("form").removeClass("hidden");
+		},
+		'click h4 .dislike': function() {
+			console.log("disliking " + this._id);
+			TopicList.update(this._id, {$inc: {dislikes: -1} });
+		},
+		'click h4 .like': function() {
+			console.log("liking " + this._id);
+			TopicList.update(this._id, {$inc: {dislikes: +1} });
+		}
+	});
+
+	Template.newTopic.events({
+		'submit form': function (event) {
+			event.preventDefault();
+			TopicList.insert({
+				'title': event.target.topicTitle.value,
+				'description': event.target.topicDescription.value,
+				'dislikes': 0
+			});
+			event.target.topicTitle.value = "";
+			event.target.topicDescription.value = "";
+		}
+	});
+
+	Template.editTopic.events({
+		'submit form': function (event) {
+			event.preventDefault();
+			console.log("saving " + this._id);
+			TopicList.update(this._id, {$set: {
+				'title': event.target.topicTitle.value,
+				'description': event.target.topicDescription.value
+			}});
+			var form = event.target;
+			$(form).prev("div.topic").removeClass("hidden");
+			$(form).addClass("hidden");
+		},
+		'click .cancel': function (event) {
+			var form = event.target.parentElement;
+			$(form).prev("div.topic").removeClass("hidden");
+			$(form).addClass("hidden");
+		}
+	});
 }
 
 if (Meteor.isServer) {
-  /*
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-  */
+	/*
+		 Meteor.startup(function () {
+	// code to run on server at startup
+	});
+	*/
 }
 
